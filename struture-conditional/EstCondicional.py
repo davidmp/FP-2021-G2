e5822c6b1b72ccd0936dec1265f8a034bb903f26
davidmamani <>
2021-05-03 15:22:34 +0000

    ejemplo 3.8

--- struture-conditional/EstCondicional.py
+++ struture-conditional/EstCondicional.py
@@ -47,5 +47,31 @@ def muestraMenorEdad():
   #Datos de Salida
   print("La(a) persona(s)", pnombre, " tiene(n):", pedad) 
 
+def calcularBono():
+  #Defenir Variables
+  bonoSueldo, bonoantiguedad, bonoMayor=0.0,0.0,0.0
+  #Datos de entrada
+  anhoAntiguadad=int(input("Ingrese Anho Antiguadad:"))
+  salario=float(input("Ingrese el salario actual:"))
+  #Proceso
+  if anhoAntiguadad>2 and anhoAntiguadad<5:
+    bonoantiguedad=salario*0.20
+  elif anhoAntiguadad>=5:
+    bonoantiguedad=salario*0.30
+
+  if salario<1000:
+    bonoSueldo=salario*0.25
+  elif salario>=1000 and salario<=3500:
+    bonoSueldo=salario*0.15
+  else:
+    bonoSueldo=salario*0.10
+
+  if bonoantiguedad>bonoSueldo:
+    bonoMayor=bonoantiguedad
+  else bonoSueldo>bonoantiguedad:
+    bonoMayor=bonoSueldo
+  #Datos de Salida
+  print("El bono mayor es:", bonoMayor)
+
 #ejercicio01()
 muestraMenorEdad()
