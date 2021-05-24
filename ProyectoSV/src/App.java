40770eb985d6a507289db4a14a698d0e89b788ee
davidmp <mamanipari@gmail.com>
2021-05-24 16:07:17 -0500

    Creando el Proyecto

---
+++ ProyectoSV/src/App.java
@@ -0,0 +1,13 @@
+import pe.edu.upeu.SubProgramas;
+
+public class App {
+    
+
+    public static void main(String[] args) throws Exception {
+        SubProgramas objSP=new SubProgramas();
+        
+        System.out.println("Hello, World! ");
+        objSP.funcionExponencial();
+        //new SubProgramas().funcionExponencial();
+    }
+}
