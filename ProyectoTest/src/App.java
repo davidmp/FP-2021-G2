04906125eb773dc8c5c0b17bb4f30cabc146e895
davidmp <mamanipari@gmail.com>
2021-05-26 09:58:17 -0500

    Ejemplo Subprograma

---
+++ ProyectoTest/src/App.java
@@ -0,0 +1,10 @@
+import pe.edu.upeu.OperacionesMat;
+
+public class App {
+    
+    public static void main(String[] args) throws Exception {        
+        System.out.println("*******Operaciones Basicas********");
+        OperacionesMat objOB=new OperacionesMat();//Un objeto
+        objOB.operacionesBasicas();
+    }
+}
