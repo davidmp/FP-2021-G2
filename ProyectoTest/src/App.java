d83c505e380a6bfdbb2a1a98a1fd8c78d3887194
davidmp <mamanipari@gmail.com>
2021-05-31 10:17:57 -0500

    Ejemplo de factorial recursivo

--- ProyectoTest/src/App.java
+++ ProyectoTest/src/App.java
@@ -1,10 +1,18 @@
 import pe.edu.upeu.OperacionesMat;
+import pe.edu.upeu.recur.SubProgramas;
+import pe.edu.upeu.util.Teclado;
 
 public class App {
     
     public static void main(String[] args) throws Exception {        
         System.out.println("*******Operaciones Basicas********");
-        OperacionesMat objOB=new OperacionesMat();//Un objeto
-        objOB.operacionesBasicas();
+        /*OperacionesMat objOB=new OperacionesMat();//Un objeto
+        objOB.operacionesBasicas();*/
+        Teclado tec=new Teclado();
+        SubProgramas objSP=new SubProgramas();
+        int numero=tec.leer(0, "Ingrese un numero");
+        System.out.println("Factorial Iterativo:"+objSP.factorialBig(numero));
+        System.out.println("Factorial Recursivo:"+objSP.factorialRecurvBig(numero));
+
     }
 }
--- ProyectoTest/src/pe/edu/upeu/recur/subProgramas.java
+++ ProyectoTest/src/pe/edu/upeu/recur/subProgramas.java
@@ -1,5 +1,35 @@
 package pe.edu.upeu.recur;
 
-public class subProgramas {
+import java.math.BigInteger;
+
+public class SubProgramas {
     
+    public long factorial(int numero) { 
+        long resultF=1;
+        if(numero>1){
+            for (int i = 1; i <= numero; i++) {
+                resultF*=i;
+            }
+        }   
+        return resultF;
+    }
+    public BigInteger factorialBig(int numero) { 
+        BigInteger resultF=new BigInteger("1");
+        if(numero>1){
+            for (int i = 1; i <= numero; i++) {
+                resultF=resultF.multiply(new BigInteger(String.valueOf(i)));
+            }
+        }   
+        return resultF;
+    }    
+
+    public BigInteger factorialRecurvBig(int numero) {
+        BigInteger resultF=new BigInteger("1");
+        if(numero>1){
+            return factorialRecurvBig(numero-1).multiply(new BigInteger(String.valueOf(numero)));          
+        } else{
+            return resultF; 
+        }                        
+    }
+
 }
