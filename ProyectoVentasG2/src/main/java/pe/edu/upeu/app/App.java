80f2d59b9bb05d00340ba92845a93e33970e30a4
davidmp <mamanipari@gmail.com>
2021-06-30 08:06:36 -0500

    Menu de opciones

--- ProyectoVentasG2/src/main/java/pe/edu/upeu/app/App.java
+++ ProyectoVentasG2/src/main/java/pe/edu/upeu/app/App.java
@@ -17,13 +17,40 @@ public class App {
         System.out.println("IDCateg:"+categ.getIdCateg()+"\tNombre:"+categ.getNombre());
     }
 
+    public static void menuMain(){
+        String mensaje="Seleccion el algoritmo que desea Ejecutar:"+
+        "\n1=Registrar Categoria"+
+        "\n12=Reportar Categoria "+
+        "\n2=Registrar Productos"+
+        "\n21=Reportar Producto"+
+        "\n22=Modificar Producto"+
+        "\n3=Realizar Venta"+
+        "\n0=Salir del programa";
+        LeerTeclado lt=new LeerTeclado(); 
+        CategoriaDao daoC;       
+        int opcion=0;
+        do{
+            opcion=lt.leer(0, mensaje);
+            switch(opcion){
+                case 1:
+                daoC=new CategoriaDao(); daoC.crearCategoria(); break;
+                case 12: 
+                daoC=new CategoriaDao(); daoC.reporteCategoria(); break;    
+                case 2: break;
+                case 3: break;
+                case 4: break;
+                case 5: break;
+                default: System.out.println("La opcion que eligio no exuiste!");
+                break;
+            }
+            if(opcion!=0)
+            System.out.println("\n Desea seguir probando: "+mensaje);
+            
+        }while(opcion!=0);        
+    }
+
     public static void main( String[] args ){
-        System.out.println( "Hello World!" );        
-        LeerTeclado lt=new LeerTeclado();
-        CategoriaDao daoC=new CategoriaDao();
-        //daoC.crearCategoria();
-        daoC.reporteCategoria();
-                
+        menuMain();       
         //new MainGUI();
     }
 }
